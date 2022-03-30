d840b0c6c6bac4d23045ecf49bb1ab2436a5ef08
Mike Vardy <mike.vardy@redspace.com>
2022-03-30 07:13:54 -0400

    tweaks

--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder } from "@nestjs/swagger";
 export const swaggerPath = "api";
 
 export const swaggerDocumentOptions = new DocumentBuilder()
-  .setTitle("My app-1")
+  .setTitle("Demo Ordering App")
   .setDescription(
     '\n\n## Congratulations! Your application is ready.\n  \nPlease note that all endpoints are secured with JWT Bearer authentication.\nBy default, your app comes with one user with the username "admin" and password "admin".\nLearn more in [our docs](https://docs.amplication.com)'
   )
@@ -17,5 +17,5 @@ export const swaggerSetupOptions = {
   },
   customCssUrl: "../swagger/swagger.css",
   customfavIcon: "../swagger/favicon.png",
-  customSiteTitle: "My app-1",
+  customSiteTitle: "Ordering App",
 };
